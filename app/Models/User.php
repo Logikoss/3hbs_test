0a7b79d5268776346a689d34d56c20db3973ac10
Cesar Perez <cpjbugg.8@gmail.com>
2026-04-27 14:29:29 -0500

    new changes
    add comments and documentation

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * Class User
+ * My class
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -40,4 +44,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function myfuction(){
+
+    }
 }
